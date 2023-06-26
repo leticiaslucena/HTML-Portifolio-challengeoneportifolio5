79988e6c56421cac7eb1f64afa65cbf04610ffa2
leticiaslucena <leticiasantos.lucena@hotmail.com>
2023-06-26 11:01:32 -0300

    first commit

---
+++ assets/js/contato.js
@@ -0,0 +1,104 @@
+const submitButton = document.querySelector('.btn-submit');
+
+let isValidationExecuted = false;
+
+function limparCampos(form) {
+    const inputElements = form.querySelectorAll('input, textarea');
+
+    inputElements.forEach((input) => {
+        input.value = '';
+    });
+}
+
+function verificarCampos(event) {
+    event.preventDefault();
+
+    if (isValidationExecuted) {
+        return;
+    }
+
+    isValidationExecuted = true;
+
+    const form = event.target.form;
+    const inputElements = form.querySelectorAll('input[required], textarea[required]');
+
+    let hasErrors = false;
+    for (let i = 0; i < inputElements.length; i++) {
+        const input = inputElements[i];
+        const errorSpan = input.nextElementSibling;
+
+        if (input.value.trim() === '') {
+            if (!errorSpan || !errorSpan.classList.contains('error-message')) {
+                const newErrorSpan = document.createElement('span');
+                newErrorSpan.className = 'error-message';
+                newErrorSpan.textContent = 'Campo obrigatório';
+                input.parentNode.insertBefore(newErrorSpan, input.nextElementSibling);
+            }
+            hasErrors = true;
+        } else if (input.name === 'name' && input.value.length > 50) {
+            if (!errorSpan || !errorSpan.classList.contains('error-message')) {
+                const newErrorSpan = document.createElement('span');
+                newErrorSpan.className = 'error-message';
+                newErrorSpan.textContent = 'Nome deve ter no máximo 50 caracteres';
+                input.parentNode.insertBefore(newErrorSpan, input.nextElementSibling);
+            }
+            hasErrors = true;
+        } else if (input.name === 'subject' && input.value.length > 50) {
+            if (!errorSpan || !errorSpan.classList.contains('error-message')) {
+                const newErrorSpan = document.createElement('span');
+                newErrorSpan.className = 'error-message';
+                newErrorSpan.textContent = 'Assunto deve ter no máximo 50 caracteres';
+                input.parentNode.insertBefore(newErrorSpan, input.nextElementSibling);
+            }
+            hasErrors = true;
+        } else if (input.name === 'email') {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(input.value)) {
+                if (!errorSpan || !errorSpan.classList.contains('error-message')) {
+                    const newErrorSpan = document.createElement('span');
+                    newErrorSpan.className = 'error-message';
+                    newErrorSpan.textContent = 'Email inválido';
+                    input.parentNode.insertBefore(newErrorSpan, input.nextElementSibling);
+                }
+                hasErrors = true;
+            } else if (errorSpan && errorSpan.classList.contains('error-message')) {
+                errorSpan.remove();
+            }
+        } else if (input.name === 'message' && input.value.length > 300) {
+            if (!errorSpan || !errorSpan.classList.contains('error-message')) {
+                const newErrorSpan = document.createElement('span');
+                newErrorSpan.className = 'error-message';
+                newErrorSpan.textContent = 'Mensagem deve ter no máximo 300 caracteres';
+                input.parentNode.insertBefore(newErrorSpan, input.nextElementSibling);
+            }
+            hasErrors = true;
+        } else if (errorSpan && errorSpan.classList.contains('error-message')) {
+            errorSpan.remove();
+        }
+    }
+
+    if (!hasErrors) {
+        setTimeout(function () {
+            alert('Mensagem enviada com sucesso!');
+            limparCampos(form);
+            submitButton.textContent = 'Enviar mensagem';
+
+            isValidationExecuted = false;
+        }, 1000);
+
+        submitButton.textContent = 'Enviando...';
+    }
+}
+
+submitButton.addEventListener('click', verificarCampos);
+
+const inputElements = document.querySelectorAll('input[required], textarea[required]');
+for (let i = 0; i < inputElements.length; i++) {
+    const input = inputElements[i];
+    input.addEventListener('input', function () {
+        const errorSpan = this.nextElementSibling;
+        if (errorSpan && errorSpan.classList.contains('error-message')) {
+            errorSpan.remove();
+        }
+    });
+}
---
+++ assets/js/scroll.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("scroll-link");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const target = document.querySelector(link.getAttribute("href"));
+
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+});
+
+window.onscroll = function () { mostrarOcultarBotao() };
+
+function mostrarOcultarBotao() {
+  const botao = document.querySelector('.volta-menu');
+
+  if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+    botao.style.display = "flex";
+  } else {
+    botao.style.display = "none";
+  }
+}
